6907839fb5d822a9dbef0a00c2ed806ab693b67e
ccioloca <catalin.cioloca@gmail.com>
2024-03-03 11:03:37 +0200

    remove firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvoU_FYVosYszCOim0pn5edETxqMHnoVA",
-  authDomain: "wdmauth.firebaseapp.com",
-  projectId: "wdmauth",
-  storageBucket: "wdmauth.appspot.com",
-  messagingSenderId: "379330790660",
-  appId: "1:379330790660:web:69f015d456f1533f06fc51"
+
 };
  
 // Initialize Firebase
